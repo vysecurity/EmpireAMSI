7b9312ef55a3a7e7ebb781dd6c1c6d13bead88af
Vincent Yiu <vysec.private@gmail.com>
2018-11-25 12:55:08 +0800

    Add PSH install script

---
+++ setup/install_psh.sh
@@ -0,0 +1,4 @@
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+sudo apt-get install -y powershell
